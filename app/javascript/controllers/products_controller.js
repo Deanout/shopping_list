7a2ae88330db310c9694c329f3a6d9573bb4aa68
Dean DeHart <DeHartDean@gmail.com>
2023-04-14 08:30:26 -0400

    E2

--- app/javascript/controllers/products_controller.js
+++ app/javascript/controllers/products_controller.js
@@ -1,77 +1,91 @@
 import { Controller } from "@hotwired/stimulus";
 
-// Connects to data-controller="products"
 export default class extends Controller {
   connect() {
-    function formatCurrency(value) {
-      return new Intl.NumberFormat("en-US", {
-        style: "currency",
-        currency: "USD",
-      }).format(value);
-    }
+    this.setupEditableFields();
+    this.setupPurchasedCheckboxes();
+  }
 
-    // Update the product's name or price when the contenteditable field loses focus
-    document.querySelectorAll(".editable").forEach((editable) => {
-      editable.addEventListener("blur", async function () {
-        const model = this.dataset.model;
-        const attribute = this.dataset.attribute;
-        const url = this.dataset.url;
-        let value = this.textContent.trim();
+  async updateFieldValue(url, attribute, value) {
+    const data = new FormData();
+    data.append(attribute, value);
 
-        if (attribute === "price") {
-          value = parseFloat(value.replace(/[^0-9.]+/g, ""));
-        }
+    const csrfToken = document.querySelector("meta[name='csrf-token']").content;
 
-        const data = new FormData();
-        data.append(`${model}[${attribute}]`, value);
-
-        const csrfToken = document.querySelector(
-          "meta[name='csrf-token']"
-        ).content;
-        const response = await fetch(url, {
-          method: "PATCH",
-          headers: { "X-CSRF-Token": csrfToken },
-          body: data,
-          credentials: "same-origin",
-        });
-
-        const result = await response.json();
-        if (result.status === "error") {
-          alert(result.message);
-        } else {
-          if (attribute === "price") {
-            this.textContent = formatCurrency(value);
-            location.reload();
-          }
-        }
-      });
+    const response = await fetch(url, {
+      method: "PATCH",
+      headers: { "X-CSRF-Token": csrfToken },
+      body: data,
+      credentials: "same-origin",
     });
 
+    const result = await response.json();
+
+    if (result.status === "error") {
+      alert(result.message);
+    } else {
+      return result;
+    }
+  }
+
+  async updateField(editable) {
+    const model = editable.dataset.model;
+    const attribute = editable.dataset.attribute;
+    const url = editable.dataset.url;
+    let value = editable.textContent.trim();
+
+    if (attribute === "price") {
+      value = parseFloat(value.replace(/[^0-9.]+/g, ""));
+    }
+
+    const result = await this.updateFieldValue(
+      url,
+      `${model}[${attribute}]`,
+      value
+    );
+
+    if (result && attribute === "price") {
+      this.updatePriceField(editable, value);
+      location.reload();
+    }
+  }
+
+  updatePriceField(editable, value) {
+    const formattedValue = this.formatCurrency(value);
+    editable.textContent = formattedValue;
+  }
+
+  formatCurrency(value) {
+    return new Intl.NumberFormat("en-US", {
+      style: "currency",
+      currency: "USD",
+    }).format(value);
+  }
+
+  setupEditableFields() {
+    document.querySelectorAll(".editable").forEach((editable) => {
+      editable.addEventListener("blur", () => this.updateField(editable));
+    });
+  }
+
+  async updatePurchasedCheckbox(url, purchased) {
+    const result = await this.updateFieldValue(
+      url,
+      "product[purchased]",
+      purchased
+    );
+
+    if (result) {
+      location.reload();
+    }
+  }
+
+  setupPurchasedCheckboxes() {
     document.querySelectorAll(".purchased_checkbox").forEach((checkbox) => {
-      checkbox.addEventListener("change", async function () {
-        const url = this.dataset.url;
-        const purchased = this.checked;
-
-        const data = new FormData();
-        data.append("product[purchased]", purchased);
-
-        const csrfToken = document.querySelector(
-          "meta[name='csrf-token']"
-        ).content;
-        const response = await fetch(url, {
-          method: "PATCH",
-          headers: { "X-CSRF-Token": csrfToken },
-          body: data,
-          credentials: "same-origin",
-        });
-
-        const result = await response.json();
-        if (result.status === "error") {
-          alert(result.message);
-        } else {
-          // Refresh the page to update the totals after marking a product as purchased or remaining
-          location.reload();
-        }
+      checkbox.addEventListener("change", () => {
+        const url = checkbox.dataset.url;
+        const purchased = checkbox.checked;
+        this.updatePurchasedCheckbox(url, purchased);
       });
     });
   }
